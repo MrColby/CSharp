a82198ee2c07cbbd4f5c85daeebd8f3e42176840
MrColby <logan.colby@gmail.com>
2023-03-06 10:26:27 -0600

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace com.logancolby.CSharp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("CSharp!");
+    }
+}
