df109d380aa2cb6020860786ba89f55b9e3135bc
MrColby <logan.colby@gmail.com>
2023-03-09 19:49:56 -0600

    Exception

--- Exceptions.cs
+++ Exceptions.cs
@@ -9,14 +9,55 @@ using System.Security.Cryptography.X509Certificates;
 
 /**
  * https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/exceptions/
+ * 
+ * use 'using' : using (var streamReader = new StreamReader(@"C:\file.zip")
+ * and the IDisposable will automatically be released.
  */
 public class Exceptions : IFunctionDriver {
+
+    public class LoganException : Exception
+    {
+        public LoganException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+    }
 
     public bool driveFunction()
     {
         LoggerBase.INSTANCE.enter();
 
+        try
+        {
+            double num = 5.0;
+            double den = 0.0;
+            double dec = num / den;
+            Console.WriteLine("? : " + dec); // doubles/floats go to infinity! Bizarre.
+            int a = 5; int b = 0; int c = a / b;
+        }
+        // because Exception would catch everything, it has to go at the end . . . : )
+        catch (DivideByZeroException ex)
+        {
+            LoggerBase.INSTANCE.msg("Divide by 0");
+            throw new LoganException("Ha ha", ex);
+        }
+        catch (ArithmeticException ex)
+        {
+            LoggerBase.INSTANCE.msg("AE");
+        }
+        catch (Exception ex)
+        {
+            LoggerBase.INSTANCE.msg("EX");
+        }
+        finally
+        {
+            // to dispose of IDisposable resources, etc. 
+            // This will always be invoked even in the case of exceptions.
+            Console.WriteLine("Finally.");
+        }
 
+        // or alternatively, use the using keyword (see main)
 
 
 
